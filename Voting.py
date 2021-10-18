91f483d63a80dc0758b0674e300b886f4360af89
Eric <85941655+EMedina456@users.noreply.github.com>
2021-10-18 09:49:07 -0400

    committing all python practice files

--- Voting.py
+++ Voting.py
@@ -1,11 +1,12 @@
 # Determine whether a given person is old enough to vote
+import re
 
 f = open("votes.txt", "r")
 numVotes = int(f.readline())
 
 for i in range(0, numVotes):
     birthday = f.readline()
-    year, month, day = birthday.split(" ")
+    year, month, day = re.split(' |-', birthday)
     if int(year) < (2007 - 18):
         print("Yes")
     elif int(year) == (2007 - 18):
